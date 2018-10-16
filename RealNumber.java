043cfa4d18ab3c09800e064cd3b53be9ce4e3358
ElizaHlglh <ewu4@stuy.edu>
2018-10-16 11:45:27 -0400

    RationalNumber.java

--- RealNumber.java
+++ RealNumber.java
@@ -1,5 +1,5 @@
 public class RealNumber{
-
+/*
   private double num;
 
   public RealNumber(){
@@ -17,4 +17,46 @@ public class RealNumber{
     return "" + num;
   }
 
+}
+*/
+
+private double value;
+
+public RealNumber(double v){
+  value = v;
+}
+
+/*
+*Return the sum of this and the other
+*/
+public double add(RealNumber other){
+  return value + other.getValue();
+}
+/*
+*Return the product of this and the other
+*/
+public double multiply(RealNumber other){
+  return value * other.getValue();
+}
+/*
+*Return the this divided by the other
+*/
+public double divide(RealNumber other){
+  return value / other.getValue();
+}
+/*
+*Return the this minus the other
+*/
+public double subtract(RealNumber other){
+  return 0;
+}
+
+
+public double getValue(){
+  return value;
+}
+
+public String toString(){
+  return ""+value;
+}
 }
